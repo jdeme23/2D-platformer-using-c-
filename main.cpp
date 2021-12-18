24f165b703ff9cef4fa2504c466a6ea4ef800a14
Nextgun <hmmarrero38@gmail.com>
2021-12-17 22:38:37 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// 2D Platformer using c++ 
+// using olcPixelGameEngine by Javidx9 - OneLoneCoder
+
+#include <iostream>
+#include <string>
+#include <olcConsoleGameEngine.h>
+
+
+using namespace std;
+
+
+int main()
+{
+	string comment;
+	 
+	cout << "I work" << endl;
+	cin >> comment; 
+	cout << comment << endl;
+
+	return 0;
+}
+
