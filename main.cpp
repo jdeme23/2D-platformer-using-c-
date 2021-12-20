903e1aa92ef7c3ec1f0abec36a7b508ace3977b8
Nextgun <hmmarrero38@gmail.com>
2021-12-20 00:20:12 -0600

    testing 1

--- main.cpp
+++ main.cpp
@@ -3,20 +3,85 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
+
 #include <olcConsoleGameEngine.h>
 
+class HJ_Platformer : public olcConsoleGameEngine
+{
+public:
+	HJ_Platformer()
+	{
+		m_sAppName = L"Tile Based Platform Game";
+	}
+private:
+	wstring sLevel;
+	int nLevelWidth;
+	int nLevelHeight;
 
-using namespace std;
+	float fCameraPosX = 0.0f;
+	float fCameraPosY = 0.0f;
+
+
+protected:
+	virtual bool OnUserCreate()
+	{
+		nLevelWidth = 64;
+		nLevelHeight = 16;
+
+		sLevel += L"................................................................";
+		sLevel += L"................................................................";
+		sLevel += L"................................................................";
+		sLevel += L"................................................................";
+		sLevel += L"........................##......................................";
+		sLevel += L"..........................#...##................................";
+		sLevel += L".................................#........##....................";
+		sLevel += L".....................###.............#..........................";
+		sLevel += L"..................###............#....##........................";
+		sLevel += L"................###.............................................";
+		sLevel += L"######################..#####################..#......##########";
+		sLevel += L"....................##.....#............#...........###.........";
+		sLevel += L".....................#......#############.......####............";
+		sLevel += L".....................#......................####................";
+		sLevel += L".....................#########################..................";
+		sLevel += L"................................................................";
+
+
+		return true;
+	}
+
+	virtual bool OnUserUpdate(float fElapsedTime)
+	{
+		// Draw the level
+		int nTileWidth = 16; 
+		int nTileHeight = 16;
+		int nVisibleTilesX = ScreenWidth() / nTileWidth;
+		int nVisibleTilesY = ScreenHeight() / nTileHeight;
+
+		// calculate top-leftmost visible tile
+		//		offset the camera so that it is positioned 
+		//		in the middle of the screen.
+		float fOffsetX = fCameraPosX - (float)nVisibleTilesX / 2.0f;
+		float fOffsetY = fCameraPosY - (float)nVisibleTilesY / 2.0f;
+
+
+
+		return true;
+	}
+};
+
+
+
+
 
 
 int main()
 {
-	string comment;
-	 
 	cout << "I work" << endl;
-	cin >> comment; 
-	cout << comment << endl;
-
+	
+	HJ_Platformer game;
+	if (game.ConstructConsole(160, 120, 8, 8))
+		game.Start();
 	return 0;
 }
 
